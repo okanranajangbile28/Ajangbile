0909f56499fd59ca211194a84cc0236847247dec
okanranajangbile28 <ajangbileheritage007@gmail.com>
2026-07-05 22:51:58 +0100

    Fix Cloudinary API secret environment variable

--- backend/controllers/imageHandler.ts
+++ backend/controllers/imageHandler.ts
@@ -8,7 +8,7 @@ import { Request } from 'express';
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const multerStorage = multer.memoryStorage();
