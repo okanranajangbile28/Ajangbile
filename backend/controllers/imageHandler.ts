e838d25672a18d8761562d04be902e640f530d57
okanranajangbile28 <ajangbileheritage007@gmail.com>
2026-07-05 23:42:05 +0100

    Debug Cloudinary

--- backend/controllers/imageHandler.ts
+++ backend/controllers/imageHandler.ts
@@ -11,6 +11,10 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+console.log('Cloud Name:', process.env.CLOUDINARY_CLOUD_NAME);
+console.log('API Key:', process.env.CLOUDINARY_API_KEY);
+console.log('API Secret exists:', !!process.env.CLOUDINARY_API_SECRET);
+
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (
